cd2ba611b989232263109b69326a134cc793721b
FrederikKea <fred1952@stud.kea.dk>
2020-12-21 18:54:21 +0100

    Opgave 3 og 4: lavet controller CRUD lavet, eneste der ikke virker er update

--- src/main/java/com/example/eksamen3sem/Controller/StudentController.java
+++ src/main/java/com/example/eksamen3sem/Controller/StudentController.java
@@ -4,9 +4,9 @@ import com.example.eksamen3sem.Model.Student;
 import com.example.eksamen3sem.Repository.StudentRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class StudentController {
@@ -19,21 +19,42 @@ public class StudentController {
     @GetMapping("/index")
     public String readAllStudent(Model model) {
         model.addAttribute("students", studentRepo.findAll());
-        //model.addAttribute("supervisor", supervisorRepo.findAll();
         model.addAttribute("student",new Student());
         return "student";
     }
 
-    /* //har lagt den op i den første GetMapping så behøver ikke den her
-    @GetMapping("/opretStudent")
-    public String createFormProduct(Model model) {
-        model.addAttribute("student",new Student());
-        return "student";
-    } */
-
     @PostMapping("createStudent")
     public String createStudent (@ModelAttribute Student student) {
         studentRepo.save(student);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/editStudent/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model, @ModelAttribute Student student) {
+        //Optional<Student> student = studentRepo.findById(id);
+        //model.addAttribute("students", studentRepo.findById(id));
+        //Student student = new Student();
+        //Optional<Student> student = studentRepo.findById(id);
+        //model.addAttribute("student", new Student());
+
+        model.addAttribute("student", student);
+        return "editStudent";
+    }
+
+    @PostMapping("/updateStudent/{id}")
+    public String updateStudent (@PathVariable("id") Long id, @ModelAttribute Student student, Model model) {
+        student.setId(id);
+        studentRepo.save(student);
+        //model.addAttribute("student",new Student());
+        model.addAttribute("student", studentRepo.findAll());
+
+        return "redirect:/index";
+    }
+
+    //
+    @GetMapping("deleteStudent/{id}")
+    public String deleteStudent (@PathVariable Long id) {
+        studentRepo.deleteById(id);
         return "redirect:/index";
     }
 
